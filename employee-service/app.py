734f958a5e9a9221cc0beff9c72d53faa4364a4a
Abhinav Srivatssan <abhivatssan@gmail.com>
2026-06-01 16:48:16 +0530

    added V4

--- employee-service/app.py
+++ employee-service/app.py
@@ -39,7 +39,7 @@ def home():
     conn.close()
 
     return jsonify({
-        "service": "employee-service-v3",
+        "service": "employee-service-v4",
         "hostname": socket.gethostname(),
         "employees": employees
     })
